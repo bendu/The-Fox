772cabe30cdb957649723766f4fcd06077a6cc01
Ben Du <bendu@me.com>
2014-02-14 21:03:03 -0800

    Adding the visual studio project to the version control

---
+++ What The Fox Say/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_The_Fox_Say
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
